0fda8690dab4afbaa8eb2b62c309f872ad96f71f
MisbahNayab12 <misbahnayab18@gmail.com>
2025-07-07 10:24:22 +0500

    uploaded trends component

--- dhagayshagay/app/page.tsx
+++ dhagayshagay/app/page.tsx
@@ -1,15 +1,15 @@
 import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
-
+import Trends from "@/components/Trends";
 
 export default function HomePage() {
   return (
     <>
       <NavBar />
       <Hero />
+      <Trends />
       <Footer />
-
     </>
 
   );
