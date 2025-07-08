52e78d084fc0de5039d2ed6782f7780a61047f31
Maryam Shahid <maryamshahid772@gmail.com>
2025-07-08 19:52:38 +0500

    testing

---
+++ dhagayshagay/app/cart/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Yeh ek basic About page hai.</p>
+        </div>
+    );
+}
