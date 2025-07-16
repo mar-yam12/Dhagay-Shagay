06316485bc9e22c98cf86795bf22bd289747fc9d
Maryam Shahid <maryamshahid772@gmail.com>
2025-07-16 22:54:02 +0500

    maryam

--- dhagayshagay/app/page.tsx
+++ dhagayshagay/app/page.tsx
@@ -1,8 +1,6 @@
 
 import AboutUs from "../components/AboutUs";
-import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
-import Footer from "../components/Footer";
 import Trends from "../components/Trends";
 import Upcoming from "@/components/Upcoming";
 import Collections from "@/components/Collections";
@@ -10,13 +8,11 @@ import Collections from "@/components/Collections";
 export default function Home() {
   return (
     <>
-      <NavBar />
       <Hero />
       <AboutUs />
       <Collections/>
       <Trends />
       <Upcoming />
-      <Footer />
     </>
   );
 }
